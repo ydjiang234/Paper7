4149fee8356f28ff5de4b488c3926ce38bbea16f
Yadong Jiang <yadong.jiang@fe.up.pt>
2017-11-15 22:41:14 +0000

    5

--- render_JYD.sh
+++ render_JYD.sh
@@ -1,6 +1,7 @@
+#!/bin/bash
 fileName="Paper7"
 bibPath="~/Documents/Git/BibTex"
 pdflatex $fileName.tex
-bitex $fileName -include-directory=$bibPath
+bibtex $fileName -include-directory=$bibPath
 pdflatex $fileName.tex
 pdflatex $fileName.tex
