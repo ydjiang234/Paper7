480944a4c2c58fd896eff3d1ce2e365d776b4928
Yadong Jiang <yjiang@FEUPSIG.FE.UP.PT>
2017-11-15 18:44:00 +0000

    2

--- render_FEUP.bat
+++ render_FEUP.bat
@@ -1,4 +1,8 @@
 set FileName="Paper7"
+set bib_path="D:\Google Drive\BibTeX"
 pdflatex %FileName%.tex
-rm *.aux *.out *.log 
+bibtex %FileName% -include-directory=%bib_path%
+pdflatex %FileName%.tex
+pdflatex %FileName%.tex
+rm *.aux *.out *.log *.bbl *.blg
 "C:\Program Files (x86)\Adobe\Acrobat Reader DC\Reader\AcroRd32.exe" %FileName%.pdf
